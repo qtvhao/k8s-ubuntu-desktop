2fa46743635b1eabb758ea5d56281b8f96c24124
Your Name <you@example.com>
2024-04-07 08:53:51 +0000

    Update docker-compose.sh and test-chrome.js

--- docker-compose.sh
+++ docker-compose.sh
@@ -2,4 +2,6 @@
 kompose convert -f docker-compose.yml
 docker-compose down || true
 docker-compose up --build
-docker-compose logs chrome
+docker-compose logs puppeteer
+ocker cp k8s-ubuntu-desktop_puppeteer_1:/page.pdf .
+docker cp k8s-ubuntu-desktop_puppeteer_1:/screenshot.png .
--- test-chrome.js
+++ test-chrome.js
@@ -1,12 +1,36 @@
 let puppeteer = require('puppeteer');
-let fs = require('fs');
 (async () => {
-    let connection = await puppeteer.connect({
-        browserURL: 'http://chrome:21223',
-    });
-    let page = await connection.newPage();
+    let browser;
+    while (true) {
+        let browserURL = 'http://203.0.117.3:21223';
+        let fetched;
+        console.log('Trying to connect to browser, browserURL:', browserURL);
+        try {
+            fetched = await fetch(browserURL + '/json/version');
+        }catch(e) {
+            console.log('Waiting for browser, browserURL:', browserURL);
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            continue;
+        }
+        try {
+            browser = await puppeteer.connect({
+                browserURL,
+            });
+            break;
+        } catch(e) {
+            console.log('Waiting for browser, browserURL:', browserURL);
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            continue;
+        }
+    }
+    console.log('Connected to browser');
+    let page = await browser.newPage();
     await page.goto('https://www.google.com');
-    await page.screenshot({ path: 'screenshot.png' });
-    await page.pdf({ path: 'page.pdf' });
+    console.log('Page loaded');
+    await page.screenshot({ path: '/screenshot.png' });
+    await page.pdf({ path: '/page.pdf' });
+    console.log('Screenshot and PDF saved');
     await page.close();
+    browser.close();
+    console.log('Page closed');
 })();
