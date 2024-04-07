f08f0720c8b3be2dcf2a986ee3fdd9f7f2a0cd99
Your Name <you@example.com>
2024-04-07 09:03:47 +0000

    Add error handling and cleanup in docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -xeo pipefail
 kompose convert -f docker-compose.yml
 docker-compose down || true
+rm page.pdf screenshot.png || true
 docker-compose up --build
 docker-compose logs puppeteer
-ocker cp k8s-ubuntu-desktop_puppeteer_1:/page.pdf .
+docker cp k8s-ubuntu-desktop_puppeteer_1:/page.pdf .
 docker cp k8s-ubuntu-desktop_puppeteer_1:/screenshot.png .
